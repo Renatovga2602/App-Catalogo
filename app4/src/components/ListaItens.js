a8637791a56a3f1c3b31f5c54b7db29c6408df5d
Renato <renatovalerio25@gmail.com>
2017-06-26 12:27:51 -0300

    Efetuando requisições HTTP com Axios

--- app4/src/components/ListaItens.js
+++ app4/src/components/ListaItens.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import {
-    Text,
     View
 } from 'react-native';
+import axios from 'axios';
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
+
+    ComponentWillMount() {
+        //requisição HTTP
+        axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+            .then(response => { console.log(response); })//apos a requisição ha uma função de calback
+            .catch(() => { console.log('Error ao recuperar os dados') });//tem função de calback tanto funcionando como não funcionando, cactch é de não funcionar;
+    }
+
     render() {
         return (
             <View>
