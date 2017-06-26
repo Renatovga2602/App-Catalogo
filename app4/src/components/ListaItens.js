cfd0bd29a1355b884ccd2c73d52b2f67c0ce561f
Renato <renatovalerio25@gmail.com>
2017-06-26 12:00:22 -0300

    criando o component Itens

--- app4/src/components/ListaItens.js
+++ app4/src/components/ListaItens.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
 import {
-    Text
+    Text,
+    View
 } from 'react-native';
+import Itens from './Itens';
 
 export default class ListaItens extends Component {
-
-    constructor(props) {
-        super(props);
-        console.log('Construindo a aplicação');
-    }
-
-    componentWillMount() {
-        console.log('Antes de Renderizar');
-    }
-
-    componentDidMount() {
-        console.log('Depois da renderização');
-    }
-
     render() {
-        console.log('Objeto é renderizado');
         return (
-            <Text>Importação de componente</Text>
+            <View>
+                <Itens />
+                <Itens />
+                <Itens />
+            </View>
         );
     }
 }
