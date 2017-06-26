73a8e33e8141ff0936cbd6fb2a742d4f81944db8
Renato <renatovalerio25@gmail.com>
2017-06-26 13:53:52 -0300

    Adicionando Style

--- app4/src/components/Itens.js
+++ app4/src/components/Itens.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
     Text,
     View,
-    Image
+    Image,
+    StyleSheet
 } from 'react-native';
 
 export default class Itens extends Component {
@@ -10,13 +11,48 @@ export default class Itens extends Component {
 
     render() {
         return (
-            <View>
-                <Image style={{ height: 100, width: 100 }} source={{ uri: this.props.item.foto }} />
-                <Text>{this.props.item.titulo}</Text>
-                <Text>{this.props.item.valor}</Text>
-                <Text>{this.props.item.local_anuncio}</Text>
-                <Text>{this.props.item.data_publicacao}</Text>
+            <View style={styles.item}>
+                <View style={styles.foto}>
+                    <Image style={{ height: 100, width: 100 }} source={{ uri: this.props.item.foto }} />
+                </View>
+                <View style={styles.detalhesItens}>
+                    <Text style={styles.txtTitulo}>{this.props.item.titulo}</Text>
+                    <Text style={styles.txtValor}>R$ {this.props.item.valor}</Text>
+                    <Text style={styles.txtDetalhe}>Local: {this.props.item.local_anuncio}</Text>
+                    <Text style={styles.txtDetalhe}>Data da Publicação: {this.props.item.data_publicacao}</Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#FFF',
+        borderWidth: 0.5,
+        borderColor: '#999',
+        margin: 10,
+        padding: 10,
+        flexDirection: 'row'
+    },
+    foto: {
+        width: 102,
+        height: 102
+    },
+    detalhesItens: {
+        marginLeft: 20,
+        flex: 1
+    },
+    txtTitulo: {
+        fontSize: 18,
+        color: 'blue',
+        marginBottom: 5
+    },
+    txtValor: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    txtDetalhe: {
+        fontSize: 16
+    }
+});
